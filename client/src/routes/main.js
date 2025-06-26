c0fefc2cc5712bb7840f2724526f00fdb9cac75d
GabidevIN <gabrielaquino32123@gmail.com>
2025-06-26 20:46:41 +0800

    v1

--- client/src/routes/main.js
+++ client/src/routes/main.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
-function main() {
+function Main() {
+const [User, Setuser] = useState("");
+const [Pass, SetPass] = useState("");
+
+
   return (
-    <div>main</div>
+    <>
+        <form>
+            <h1>LOGIN SYSTEM</h1>
+            <input value={User} placeholder='Enter Username' onChange={e => Setuser(e.target.value)}/>
+            <input value={Pass} placeholder='Enter Password' onChange={e => SetPass(e.target.value)}/>
+            <button>LOGIN</button>
+            <h1>MESSAGE</h1>
+
+        </form>
+    </>
   )
 }
 
-export default main
+export default Main 
