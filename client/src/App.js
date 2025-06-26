3f89c9be7f111f165ff0902025b82401e1604f67
GabidevIN <gabrielaquino32123@gmail.com>
2025-06-26 20:03:22 +0800

    v1

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,8 @@
-import React, { useEffect, useState} from 'react'
+import React from 'react'
+import MAIN from './routes/main'
 
 function App() {
-  const [datas,setdata] = useState('')
-
-  useEffect(() => {
-    fetch("/api").then(
-      response => response.json()
-    ).then(
-      data => {
-        setdata(data)
-      }
-    )
-  })
-
-
-  return (
-    <>
-    {(typeof datas.users === 'undefined') ? (
-      <p>Loading...</p>
-    ) : (
-      datas.users.map((user,i) =>(
-        <p key={i}>{user}</p>)
-      )
-    )}
-
-
-
-    </>
-  )
+  return <MAIN/>
 }
-
-
 
 export default App
