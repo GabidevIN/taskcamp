ef0722958705587758ae858747c85eb243d94892
GabidevIN <gabrielaquino32123@gmail.com>
2025-06-26 21:14:44 +0800

    v1

--- client/src/routes/main.js
+++ client/src/routes/main.js
@@ -8,8 +8,8 @@ const [Pass, SetPass] = useState("");
   return (
     <>
         <form>
-            <h1>LOGIN SYSTEM</h1>
-            <input value={User} placeholder='Enter Username' onChange={e => Setuser(e.target.value)}/>
+            <h1 className='text-7xl text-center text-blue-400'>LOGIN SYSTEM</h1>
+            <input className='bg-#23423 rounded' value={User} placeholder='Enter Username' onChange={e => Setuser(e.target.value)}/>
             <input value={Pass} placeholder='Enter Password' onChange={e => SetPass(e.target.value)}/>
             <button>LOGIN</button>
             <h1>MESSAGE</h1>
---
+++ client/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.js",], // REQUIRED ILAGAY GABO {js,jsx,ts,tsx} if needed
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
