9df1adc59991c4c90bd7d62d9e94ad74156696e6
GabidevIN <gabrielaquino32123@gmail.com>
2025-06-29 18:50:59 +0800

    v1

--- client/src/routes/main.js
+++ client/src/routes/main.js
@@ -2,13 +2,13 @@ import React, {useState, useEffect} from 'react'
 
 function Main() {
 // USER INPUT 
-const [User, Setuser] = useState("");
-const [Pass, SetPass] = useState("");
-const [result, Setresult] = useState("")
+const [User, Setuser] = useState('');
+const [Pass, SetPass] = useState('');
+const [result, Setresult] = useState('')
 // USER INPUT 
 
 const testing =() =>{
-  const testt = SetPass(Pass) + Setuser(User)
+  const testt = String(Pass) + String(User)
   Setresult(testt)
 }
 
@@ -18,13 +18,11 @@ const testing =() =>{
   return (
     // ----- LOGIN GUI
     <>
-        <form>
             <h1>LOGIN SYSTEM</h1>
             <input value={User} placeholder='Enter Username' onChange={e => Setuser(e.target.value)}/>
             <input value={Pass} placeholder='Enter Password' onChange={e => SetPass(e.target.value)}/>
             <button onClick={testing}>LOGIN</button>
             <h1>MESSAGE:{result} </h1>
-        </form>
     </>
   )
 }
