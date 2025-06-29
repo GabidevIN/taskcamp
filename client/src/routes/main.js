c4c80c5c5a4aec8e5910eca141f6e2bfd03fa07b
GabidevIN <gabrielaquino32123@gmail.com>
2025-06-29 18:03:34 +0800

    v1

--- client/src/routes/main.js
+++ client/src/routes/main.js
@@ -4,7 +4,13 @@ function Main() {
 // USER INPUT 
 const [User, Setuser] = useState("");
 const [Pass, SetPass] = useState("");
+const [result, Setresult] = useState("")
 // USER INPUT 
+
+const testing =() =>{
+  const testt = SetPass(Pass) + Setuser(User)
+  Setresult(testt)
+}
 
 
 
@@ -13,11 +19,11 @@ const [Pass, SetPass] = useState("");
     // ----- LOGIN GUI
     <>
         <form>
-            <h1 className='text-7xl text-center text-blue-400'>LOGIN SYSTEM</h1>
-            <input className='bg-#23423 rounded' value={User} placeholder='Enter Username' onChange={e => Setuser(e.target.value)}/>
+            <h1>LOGIN SYSTEM</h1>
+            <input value={User} placeholder='Enter Username' onChange={e => Setuser(e.target.value)}/>
             <input value={Pass} placeholder='Enter Password' onChange={e => SetPass(e.target.value)}/>
-            <button>LOGIN</button>
-            <h1>MESSAGE</h1>
+            <button onClick={testing}>LOGIN</button>
+            <h1>MESSAGE:{result} </h1>
         </form>
     </>
   )
