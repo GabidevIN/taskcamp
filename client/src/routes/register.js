648fdbf594a9cb3567e9bb36c201657f51082f91
GabidevIN <gabrielaquino32123@gmail.com>
2025-07-01 20:17:23 +0800

    v1

--- client/src/routes/register.js
+++ client/src/routes/register.js
@@ -1,21 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from 'react-router-dom';
 import Login from './login'
+import axios from 'axios';
 
 
-function register() {
+function Register() {
+  const [values, setValues] = useState({
+    name: '',
+    email: '',
+    pass: ''
+  });
+
+  const hanedleSubmit = (event) => {
+    event.preventDefault();
+    axios.post('')
+  }
+
   return (
     <>
-    <form>
-      <input placeholder='Enter Name'/>
-      <input placeholder='Enter Email'/>
-      <input placeholder='Enter Password'/>
+    <form onSubmit={hanedleSubmit}> 
+      <input placeholder='Enter Name' onChange={e => setValues({...values, name: e.target.value})}/>
+      <input placeholder='Enter Email' onChange={e => setValues({...values, email: e.target.value})}/>
+      <input placeholder='Enter Password' onChange={e => setValues({...values, pass: e.target.value})}/>
       <button>SIGN UP</button>
     </form>
 
     <Link to="/login">LOGIN</Link>
     </>
-  )
+  );
 }
 
-export default register
+export default Register
