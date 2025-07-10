9aafb39cc0d6c69335372bf33c1d07d35c427eac
GabidevIN <gabrielaquino32123@gmail.com>
2025-07-10 22:17:13 +0800

    v1

--- client/src/routes/home.js
+++ client/src/routes/home.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import Register from './register'
+import LOGIN from './login'
+import { Link } from 'react-router-dom';
+
+
 
 function home() {
   return (
-    <div>home</div>
+    <>
+    home
+    <Link to="/Register">Register</Link>
+    <Link to="/Login">LOGIN</Link>
+
+    </>
   )
 }
 
