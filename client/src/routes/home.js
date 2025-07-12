64bdf36ab54d3eeb9250cd5392320d266ae54be9
GabidevIN <gabrielaquino32123@gmail.com>
2025-07-12 20:58:44 +0800

    v1

--- client/src/routes/home.js
+++ client/src/routes/home.js
@@ -27,13 +27,13 @@ const closebtn = () => {
 return (
   <>
   <title>TASKCAMP</title>
-    <div className='bg-white w-screen h-screen text-center p-4 space-y-4'>
+    <div className="w-screen h-screen bg-gradient-to-r from-[#948997] to-[#393e3e] text-white p-4 rounded">
       <nav className='flex justify-center gap-4'>
         <a href="#" className='text-black bg-white rounded-e-md border border-black p-2' onClick={handleAuthClick("login") }>Login</a>
         <a href="#" className='text-black bg-white rounded-e-md border border-black p-2' onClick={handleAuthClick("register")}>Register</a>
       </nav>
 
-      <h1 className='font-bold'>ON PROGRESS</h1>
+      <h1 className='font-bold text-center'>ON PROGRESS</h1>
       <div className='flex justify-center gap-4'>
         <Link to="/Register" className='text-black bg-white'>Register</Link>
         <Link to="/Login" className='text-black bg-white '>LOGIN</Link>
@@ -41,7 +41,7 @@ return (
 
 {/*----- LOGIN FORM // HIDDEN -----*/}
 {showLogin && (
-  <div>
+  <div className='bg-white text-black p-4 rounded w-fit max-w-md mx-auto my-4'>
     <div>
       <h2>LOGIN</h2>
         <form>
@@ -56,7 +56,7 @@ return (
 
 {/*----- REGISTRATION FORM // HIDDEN -----*/}
 {showReg && (
-  <div>
+  <div className='bg-white text-black p-4 rounded w-fit max-w-md mx-auto my-4'>
     <div>
       <h2>REGIS</h2>
         <form>
