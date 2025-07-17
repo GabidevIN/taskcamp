349a6d5a0753ac178ae3ced3446f1018ebf88199
GabidevIN <gabrielaquino32123@gmail.com>
2025-07-17 22:01:58 +0800

    v1

--- client/src/routes/main.js
+++ client/src/routes/main.js
@@ -1,8 +1,28 @@
 import React from 'react'
 
 function main() {
+// ----- SESSION SYSTEM
+
+
+
+
+// ----- USER INTERFACE FOR MAIN PAGE
+
+
+
   return (
-    <div>main</div>
+    <>
+    <div className="w-screen h-screen bg-gradient-to-r from-[#948997] to-[#393e3e] text-white p-4 rounded">
+
+
+
+
+
+
+
+
+    </div>
+    </>
   )
 }
 
